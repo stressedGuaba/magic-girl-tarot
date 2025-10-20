7498e31f6d00248ca8497f357d83a1215de82e9e
unknown <pawletteGC@gmail.com>
2025-10-20 03:14:05 -0700

    for demo

--- scenes/card_ui/card_base_state.gd
+++ scenes/card_ui/card_base_state.gd
@@ -5,6 +5,10 @@ func enter() -> void:
 	if not card_ui.is_node_ready():
 		await card_ui.ready
 	
+	if card_ui.tween and card_ui.tween.is_running():
+		card_ui.tween.kill()
+	
+	
 	card_ui.reparent_requested.emit(card_ui)
 	card_ui.color.color = Color.HOT_PINK #debug
 	card_ui.state.text = "BASE"
