8049c49cb2152cb482dabe741e086cdef30350ef
unknown <pawletteGC@gmail.com>
2025-10-24 04:49:18 -0700

    enemy attack functions. commit before bugs come out TT

---
+++ custom_resources/enemy_stats.gd
@@ -0,0 +1,8 @@
+class_name EnemyStats
+extends Stats
+
+@export var ai : PackedScene
+
+## conditional (ebery x turns. under x %) 
+## chance based (x% and x% chance of happening)
+## some mat6h stufdf going on idk
